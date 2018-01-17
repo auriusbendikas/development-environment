ad05c5bacbf51149935c58f4f49c676eae6a78c3
Aurius Bendikas <aurius.bendikas@hm.com>
2018-01-17 13:43:04 +0100

    Added firefox browser support.
    * Made firefox default browser for aem machine.

---
+++ ansible-roles/firefox/files/etc/firefox/syspref.js
@@ -0,0 +1,4 @@
+// This file can be used to configure global preferences for Firefox
+// Example: Homepage
+pref("browser.startup.page", 0);
+pref("browser.startup.homepage_override.mstone", "ignore");
